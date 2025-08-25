9664675ea28e99548687fb7c4f7bccc677307ee0
Theus28238 <matheusguilherme1520@gmail.com>
2025-08-25 15:33:11 -0300

    Iniciando Prorjeto

---
+++ src/test/java/com/io/github/theus28238/HospedagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.theus28238;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospedagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
