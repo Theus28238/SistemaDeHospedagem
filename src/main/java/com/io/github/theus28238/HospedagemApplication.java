96ad51296e7f7a67257e33bd01d8cdea4e0a2ebb
Theus28238 <matheusguilherme1520@gmail.com>
2025-08-25 15:33:11 -0300

    Iniciando Prorjeto

---
+++ src/main/java/com/io/github/theus28238/HospedagemApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.theus28238;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospedagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospedagemApplication.class, args);
+	}
+
+}
