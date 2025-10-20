b6639ee295ec68cf6fe2ed4db7355881f6b806c0
pDiazz-dev <pedrodias20152015@gmail.com>
2025-10-20 16:43:35 -0300

    Listagem de hospede conectada

--- src/main/java/com/io/github/theus28238/config/CorsConfig.java
+++ src/main/java/com/io/github/theus28238/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:3000")
+                        .allowedOrigins("http://127.0.0.1:3000, http://127.0.0.1:5500")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
