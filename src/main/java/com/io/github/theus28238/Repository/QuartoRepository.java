a8b1c828d75b94e0c36ea3b080cd6e77f7c9bab7
pDiazz-dev <pedrodias20152015@gmail.com>
2025-11-10 11:47:55 -0300

    fix(Quartos-repository)- concertando erro no QUERY

--- src/main/java/com/io/github/theus28238/Repository/QuartoRepository.java
+++ src/main/java/com/io/github/theus28238/Repository/QuartoRepository.java
@@ -14,6 +14,6 @@ public interface QuartoRepository extends JpaRepository<Quartos, UUID> {
 
     void deleteByNumeroQuarto(Integer numeroQuarto);
 
-    @Query("select quarto from Quartos quarto where quarto.ativo == true")
+    @Query("select quarto from Quartos quarto where quarto.ativo = true")
     List<Quartos> findAllByAtivo(Boolean ativo);
 }
